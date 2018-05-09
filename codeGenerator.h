8f7378a95fc2821249f20bc254cf56e9ca940241
omar_swidan <oswidan97@gmail.com>
2018-05-09 23:14:43 +0200

    generation for read

--- codeGenerator.h
+++ codeGenerator.h
@@ -1,13 +1,17 @@
 //
 // Created by omar_swidan on 09/05/18.
 //
+#include <vector>
+#include <string>
 
 #ifndef COMPILER_CODEGENERATOR_H
 #define COMPILER_CODEGENERATOR_H
 
 #endif //COMPILER_CODEGENERATOR_H
+
+using namespace std;
 namespace codeGenerator{
 
-    void readGen();
+    void readGen(vector<string>&,ofstream& assemblyFile);
 
 }
